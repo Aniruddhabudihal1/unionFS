6c8cd2a87dd6acceb7cc7e22d8d0d341abb3ce77
Aniruddha Budihal <aniruddhabudihal@gmail.com>
2026-06-08 23:11:00 +0530

    feat : basic filesystem stuff working, now onto the real implementation

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,13 @@ impl Node {
             Self::File { .. } => None,
         }
     }
+
+    fn get_file_content(&self) -> Option<&String> {
+        match self {
+            Node::Directory { .. } => None,
+            Node::File { file_content } => Some(file_content),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -94,6 +101,7 @@ impl unionFS {
         }
     }
 
+    /*
     fn fill_up_hashes(&mut self, name: String, parent_inode_value: u64) -> u64 {
         println!("does fill_up_hashes even get invoked ? ");
         let new_inode_value = self.next_inode_value;
@@ -110,6 +118,7 @@ impl unionFS {
 
         new_inode_value
     }
+    */
 }
 
 impl Filesystem for unionFS {
@@ -120,50 +129,32 @@ impl Filesystem for unionFS {
         name: &std::ffi::OsStr,
         reply: fuser::ReplyEntry,
     ) {
-        // perform lookup
-        println!("inside the lookup function");
         let name_local_format = name.to_string_lossy().to_string();
         println!(
-            "The name entered into the lookup function is : {} and parent inode value is : {}",
+            "lookup function got invoked for : {} and its parent inode value is : {}",
             name_local_format, parent.0
         );
 
-        let fooo = self.mapping.iter().clone();
-        for i in fooo {
-            println!("{:?}", i);
-            println!();
-            println!();
-        }
-
         let possible_inode_content = self.mapping.get(&parent.0);
-        match possible_inode_content {
-            Some(inode_instance) => {
-                println!(
-                    "So the corresponding inode value for {} exists in {} hashmap",
-                    name_local_format, parent.0
-                );
-                let returned_children = inode_instance
-                    .node_kind
-                    .just_access_children_without_editing_anything()
-                    .unwrap();
-                let res = returned_children.get(&name_local_format);
-                match res {
-                    Some(childs_inode_value) => {
-                        println!("Does it even enter here");
-                        let res2 = self.mapping.get(childs_inode_value).unwrap();
-                        let f_attr = res2.inode_attributes;
-                        let d = Duration::from_nanos(20);
-                        reply.entry(&d, &f_attr, Generation(0))
-                    }
-                    None => {
-                        println!(
-                            "it returned NONE on the inode value, so the InodeContent exists but the inode value does not ? if that makes sense"
-                        );
-                        reply.error(Errno::ENOENT)
-                    }
-                }
+        if let Some(t) = possible_inode_content {
+            // parent exists
+            if let Some(tt) = t
+                .node_kind
+                .just_access_children_without_editing_anything()
+                .unwrap()
+                .get(&name_local_format)
+            {
+                // we found the inode value of the kid
+                let d = Duration::default();
+                let tmp = self.mapping.get(tt).unwrap();
+                reply.entry(&d, &tmp.inode_attributes, Generation(1));
+            } else {
+                println!("child does not exist ");
+                reply.error(Errno::ENOENT);
             }
-            None => reply.error(fuser::Errno::ENOENT),
+        } else {
+            println!("parent does not exist");
+            reply.error(Errno::ENOENT);
         }
     }
 
@@ -175,13 +166,12 @@ impl Filesystem for unionFS {
         offset: u64,
         mut reply: fuser::ReplyDirectory,
     ) {
-        println!("performing readdir function");
         println!(
             "The inode number on which we are performing the readdir is : {}",
             ino.0
         );
 
-        let temp = &mut self
+        let temp = &self
             .mapping
             .get(&ino.0)
             .unwrap()
@@ -198,12 +188,12 @@ impl Filesystem for unionFS {
         aggregate.push((ino.0, FileType::Directory, ".".to_string()));
         aggregate.push((ino.0, FileType::Directory, "..".to_string()));
 
-        for (i, (inode, file_type, name)) in aggregate.into_iter().enumerate().skip(offset as usize)
-        {
-            if reply.add(INodeNo(inode), (i + 1) as u64, file_type, &name) {
+        for (i, entry) in aggregate.into_iter().enumerate().skip(offset as usize) {
+            if reply.add(INodeNo(entry.0), (i + 1) as u64, entry.1, entry.2) {
                 break;
             }
         }
+        reply.ok();
     }
 
     fn getattr(
@@ -213,11 +203,45 @@ impl Filesystem for unionFS {
         _fh: Option<fuser::FileHandle>,
         reply: fuser::ReplyAttr,
     ) {
+        println!("getattr got invoked for inode value : {}", ino.0);
         let d = Duration::default();
         match self.mapping.get(&ino.0) {
-            Some(i) => reply.attr(&d, &i.inode_attributes),
-            None => reply.error(Errno::ENOENT),
+            Some(i) => {
+                println!("getattr worked ? ");
+                reply.attr(&d, &i.inode_attributes);
+            }
+            None => {
+                println!("getattr did not work");
+                reply.error(Errno::ENOENT);
+            }
         }
+    }
+
+    fn setattr(
+        &self,
+        _req: &fuser::Request,
+        ino: INodeNo,
+        _mode: Option<u32>,
+        uid: Option<u32>,
+        gid: Option<u32>,
+        size: Option<u64>,
+        _atime: Option<fuser::TimeOrNow>,
+        _mtime: Option<fuser::TimeOrNow>,
+        _ctime: Option<SystemTime>,
+        _fh: Option<fuser::FileHandle>,
+        _crtime: Option<SystemTime>,
+        _chgtime: Option<SystemTime>,
+        _bkuptime: Option<SystemTime>,
+        flags: Option<fuser::BsdFileFlags>,
+        reply: fuser::ReplyAttr,
+    ) {
+        let mut at = make_attribute(ino.0, false);
+        at.flags = flags.unwrap().bits();
+        at.size = size.unwrap();
+        at.uid = uid.unwrap();
+        at.gid = gid.unwrap();
+        let dur = Duration::default();
+        reply.attr(&dur, &at);
     }
 }
 
@@ -354,7 +378,7 @@ fn main() {
 
     let testt = list_of_directories.iter();
     for i in testt {
-        println!("tet {}", i.to_string_lossy());
+        println!("{}", i.to_string_lossy());
     }
 
     let v = vec![MountOption::RO, MountOption::AutoUnmount];
